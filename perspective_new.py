78dfb1e4d7305b27a603028e18d07ba36b525804
apoorvarpi <arpiapoorva@gmail.com>
2017-02-08 00:19:21 +0530

    Variable I/P and O/P

--- perspective_new.py
+++ perspective_new.py
@@ -26,8 +26,14 @@ def resizeOnCenter(img, factor):        # resizing image keeping it in center
 if __name__ == '__main__':
 
     # Read in the image.
-    im_src1 = cv2.imread("book1.jpg")
-    im_src2 = cv2.imread("book2.jpg")
+    im_nm1 = input("First image name: ")
+    im_cam1 = input("First camera name: ")
+    im_nm2 = input("Second image name: ")
+    im_cam2 = input("First camera name: ")
+    im_name1 = "input/"+im_cam1+"/"+im_nm1
+    im_name2 = "input/"+im_cam2+"/"+im_nm2
+    im_src1 = cv2.imread(im_name1)
+    im_src2 = cv2.imread(im_name2)
 
     # Destination image
     size = (300,400,3)
@@ -59,23 +65,23 @@ if __name__ == '__main__':
     #Show second image and wait for four clicks
     pts_src2 = get_four_points(im_src2);
 
-    #new code
     rows,cols,ch = im_src1.shape
 
     pts_src1 = np.float32(pts_src1)
     pts_src2 = np.float32(pts_src2)
 
     M = cv2.getPerspectiveTransform(pts_src2,pts_src1)
+    #saving the matrix
+    file_name = "./Matrices/"+im_cam1+"_"+im_cam2+".txt"
+    np.savetxt(file_name,M)
     im_src = cv2.warpPerspective(im_src2, M, (cols,rows))
-    
-    #new code ends here
-
     #Display new image and wait for four clicks
-    pts_src = get_four_points(im_src);
 
     print('''
             Click on the four corners
-            ''')
+        ''')
+    pts_src = get_four_points(im_src);
+
     # Calculate the homography
     h, status = cv2.findHomography(pts_src, pts_dst)
 
