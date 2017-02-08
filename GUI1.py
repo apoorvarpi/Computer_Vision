1843178806f2b440f68e66ac200b3b8843bf0ca7
apoorvarpi <arpiapoorva@gmail.com>
2017-02-08 20:38:55 +0530

    Few changes

--- GUI1.py
+++ GUI1.py
@@ -15,24 +15,17 @@ def ok():
 
 if __name__ == '__main__':
     app = Tk()
-
     app.title("Camera Calliberation")
     app.geometry("500x500")
     app.resizable(0, 0)
-
     var1 = StringVar(app)
     var1.set("Select the number of cameras")
-
     label = Label( app, textvariable=var1, relief=FLAT )
     label.pack(side=LEFT)
-
     var = IntVar(app)
     var.set(1)
-
     option = OptionMenu(app, var, 1,2,3,4,5,6)
     option.pack(side=LEFT)
-
     button = Button(app, text="OK", command=ok)
     button.pack(side=LEFT)
-
     mainloop()
--- new_window.py
+++ new_window.py
@@ -7,9 +7,7 @@ def new_win(var):
     for i in range(1,var+1):
         x = str(i)
         im_name = "input/C"+x+"/calib.jpg"
-        print (im_name)
         image = cv2.imread(im_name)
         im_title = "Camera "+x
-        tt = cv2.imread("input/C1/calib.jpg")
         cv2.imshow(im_title,image)
         cv2.waitKey(0)
