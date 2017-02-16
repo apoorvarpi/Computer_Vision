9f57ea10065542cce97a6e652e83a2b05db884d0
apoorvarpi <arpiapoorva@gmail.com>
2017-02-16 16:25:05 +0530

    Still checking threshold value, basics done

--- color_segmentation.py
+++ color_segmentation.py
@@ -1,12 +1,27 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 
 def threshold():
-    img1 = cv2.imread('book1.jpg')
-    img = cv2.cvtColor(img1, cv2.COLOR_BGR2YCR_CB)
-    cv2.imshow("Images", img1)
-    cv2.imshow("YCBCR", img)
-    cv2.waitKey(0)
+    for t in range(1,8):
+        print(t)
+        name = './input/img'+str(t)+'.jpg'
+        img1 = cv2.imread(name)
+        img = cv2.cvtColor(img1, cv2.COLOR_BGR2YCR_CB)
+        Y,Cb,Cr = cv2.split(img)
+        rows, cols = Y.shape
+        for i in range(0,rows):
+            for j in range(0,cols):
+                a = int(Cb[i][j])
+                b = int(Cr[i][j])
+                c = int(Y[i][j])
+                x = math.sqrt(a*a + b*b + c*c)
+                if x > 225:
+                    Y[i][j] = 255
+                else:
+                    Y[i][j] = 0
+        cv2.imshow("Image",Y)
+        cv2.waitKey(0)
 
 threshold()
